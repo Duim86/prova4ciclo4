a2f41c853b57c697a93f09adbcef4484bb448c6c
Duim86 <zanlorenzi@gmail.com>
2021-12-14 22:49:37 -0300

    cria tabelas banco

---
+++ backend/src/main/resources/db/migration/V20211214223122__create-table-tipo_produto.sql
@@ -0,0 +1,6 @@
+create table tipo_produto
+(
+    id   bigint      not null auto_increment,
+    nome varchar(150) not null unique,
+    primary key (id)
+);
---
+++ backend/src/main/resources/db/migration/V20211214223221__create-table-produto.sql
@@ -0,0 +1,15 @@
+create table produto
+(
+    id              bigint         not null auto_increment,
+    nome            varchar(150)   not null,
+    preco_venda     decimal(10, 2) not null,
+    preco_compra    decimal(10, 2) not null,
+
+    fornecedor_id   bigint         not null,
+    tipo_produto_id bigint         not null,
+
+    constraint fk_produto_fornecedor foreign key (fornecedor_id) references fornecedor (id),
+    constraint fk_produto_tipo_produto foreign key (tipo_produto_id) references tipo_produto (id),
+
+    primary key (id)
+);
